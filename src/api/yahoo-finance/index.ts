3900a518c81521834f3532f3953770359ffa9e56
= <michael.w.murphy@outlook.com>
2020-06-22 06:04:43 +0100

    Improvement to fetch error handling

--- src/api/yahoo-finance/index.ts
+++ src/api/yahoo-finance/index.ts
@@ -1,6 +1,6 @@
-import { Symbol } from "../../types"
+import { Symbol, SymbolSummary, FinanceInfo } from "../../types"
 
-export const search = async (value: string): Promise<Symbol | []> => {
+export const search = async (value: string): Promise<Symbol[] | null> => {
 	if (value.length > 0) {
 		const res = await fetch(`http://localhost:8181/yf/api/finance/search?q=${value}`, {
 			headers: {
@@ -12,15 +12,19 @@ export const search = async (value: string): Promise<Symbol | []> => {
 			const data = await res.json()
 			return data.results
 		} else {
-			return []
+			return null
 		}
 	} else {
-		return []
+		return null
 	}
 }
 
-export const getFinanceInfo = async (symbol: string) => {
-	const url = `http://localhost:8181/yf/api/finance/chart/${symbol}?region=GB&lang=en-GB&includePrePost=false&interval=2m&range=1d&corsDomain=uk.finance.yahoo.com&.tsrc=finance`
+export const getFinanceInfo = async (
+	symbol: string,
+	interval: string = "2m",
+	range: string = "1d"
+): Promise<FinanceInfo | null> => {
+	const url = `http://localhost:8181/yf/api/finance/chart/${symbol}?region=GB&lang=en-GB&includePrePost=false&interval=${interval}&range=${range}&corsDomain=uk.finance.yahoo.com&.tsrc=finance`
 
 	try {
 		const res = await fetch(url, {
@@ -35,14 +39,18 @@ export const getFinanceInfo = async (symbol: string) => {
 		} else {
 			console.log("There is an issue with the response")
 			console.log(res)
+
+			return null
 		}
 	} catch (err) {
 		console.log("There has been an issue fetching stock information")
 		console.error(err)
+
+		return null
 	}
 }
 
-export const getSummary = async (symbol: string) => {
+export const getSummary = async (symbol: string): Promise<SymbolSummary | null> => {
 	const url = `http://localhost:8181/yf/api/finance/quoteSummary/${symbol}`
 
 	try {
@@ -50,13 +58,17 @@ export const getSummary = async (symbol: string) => {
 
 		if (res && res.ok) {
 			const data = await res.json()
-			return data
+			return data as SymbolSummary
 		} else {
 			console.log("There is an issue with the response")
 			console.log(res)
+
+			return null
 		}
 	} catch (err) {
 		console.log("There has been an issue fetching stock information")
 		console.error(err)
+
+		return null
 	}
 }
